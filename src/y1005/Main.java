8d97e064f7feec5714d19b5a3058dd8175dfff2e
aweffr <aweffr@qq.com>
2016-09-06 12:57:29 +0800

    Rewrite the y1005 code and document.

--- src/y1005/Main.java
+++ src/y1005/Main.java
@@ -1,70 +1,83 @@
 package y1005;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Accepted! 09/26, 12:54pm
+ * @author aweff
+ *
+ */
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
+	public static void main(String[] args) throws IOException, NumberFormatException {
+		Reader.init(System.in);
+		HashSet<Integer> set = new HashSet<Integer>();
+		int N = Reader.nextInt();
+		int[] data = new int[N];
 		
-		ArrayList<Integer> numberLst = new ArrayList<Integer>();
-		
-		for (int i=0; i<N; i++) 
-		{
-			numberLst.add(in.nextInt());
-		}
-		in.close();
-		
-		boolean[] isDuplicated = new boolean[N];
-		
-		for (int i=0; i<N; i++) {
-			int num_temp = numberLst.get(i);
-			if (isDuplicated[i])
-			{
-				continue;
-			}
-			while (num_temp != 1) {
-				if (num_temp%2 == 0)
-				{
-					num_temp /= 2;
-				}
-				else
-				{
-					num_temp = (3*num_temp + 1) / 2;
-				}
-				if (numberLst.contains(num_temp))
-				{
-					isDuplicated[numberLst.indexOf(num_temp)] = true;
-				}
+		for(int i=0; i<N; i++) {
+			int temp = Reader.nextInt();
+			data[i] = temp;
+			while(temp != 1) {
+				temp = nextStep(temp);
+				boolean successAdd = set.add(temp);
+				if (!successAdd) break;
 			}
 		}
 		
-		ArrayList<Integer> outLst = new ArrayList<Integer>();
-		for (int i=0; i<N; i++)
-		{
-			if (!isDuplicated[i])
-			{
-				outLst.add(-numberLst.get(i));
-			}
-		}
-		Collections.sort(outLst);
+//		System.out.println(set);
 		
-		for (int i=0; i<outLst.size(); i++)
-		{
-			System.out.print(-outLst.get(i));
-			if ( i<(outLst.size()-1) )
-			{
+		for(int i=0; i<N; i++) {
+			if ( set.contains(data[i]) ) {
+				// data[i] is not a independent number
+				data[i] = -1;
+			}
+		} // After this for loop, all the independent number is set to -1.
+		
+		Arrays.sort(data);
+//		System.out.println(Arrays.toString(data));
+		
+		for (int i=N-1; i>=0; i--) {
+			int temp = data[i];
+			if (temp != -1) {
+				System.out.print(temp);
+			}
+			if (i>0 && data[i-1] != -1) {
 				System.out.print(" ");
 			}
 		}
+		System.out.println();
 	}
 	
-	public static int compare(int x, int y)
-	{
-		if (x<y) return 1;
-		else if(x==y) return 0;
-		else return -1;
+	public static int nextStep(int in) {
+		int out = 0;
+		if (in % 2 == 0) {
+			out = in/2;
+		}
+		else {
+			out = (3*in + 1)/2;
+		}
+//		System.out.println("nextStep("+in+") called, out= "+out);
+		return out;
+	}
+}
+
+class Reader {
+	static BufferedReader br;
+	static StringTokenizer tk;
+	
+	public static void init(InputStream in) {
+		br = new BufferedReader(new InputStreamReader(in));
+		tk = new StringTokenizer("");
+	}
+	public static String next() throws IOException {
+		while ( !tk.hasMoreTokens() ) {
+			tk = new StringTokenizer(br.readLine());
+		}
+		return tk.nextToken();
+	}
+	public static int nextInt() throws IOException, NumberFormatException {
+		return Integer.parseInt(next());
 	}
 }
